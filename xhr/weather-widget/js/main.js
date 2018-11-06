e9535b419895bb4d773dbba83ea2ef986ffbf6d0
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-06 18:19:55 +0300

    made weather-widget`s task

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,14 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
-request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
+request.addEventListener('load', onLoad);
+request.addEventListener('error', () => console.log("Сетевая ошибка."));
+function onLoad() {
+	if (request.status === 200) {
+	  const response = JSON.parse(request.responseText);
+	  setData(response);
+	} else {
+	  console.log(`Ошибка: ${request.status}: ${request.statusText}`);
+	}
 }
+request.send();
+
