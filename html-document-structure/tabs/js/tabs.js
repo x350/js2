23682b2e502880b85ffc9077dd640bd16b355880
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-12 16:34:37 +0300

    made tabs task

---
+++ html-document-structure/tabs/js/tabs.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const tabsNav = document.querySelector('.tabs-nav');
+const tab = tabsNav.firstElementChild;
+tabsNav.removeChild(tab);
+const articleList = document.getElementsByClassName('tabs-content')[0].children;
+// console.log(articleList);
+Array.from(articleList).forEach(item => {
+	let newTab = tab.cloneNode(true);
+	newTab.firstElementChild.innerText = item.dataset.tabTitle;
+	newTab.firstElementChild.classList.add(item.dataset.tabIcon);
+	tabsNav.appendChild(newTab);
+});
+tabsNav.firstElementChild.classList.add('ui-tabs-active');
+let title = tabsNav.firstElementChild.textContent;
+Array.from(articleList).forEach(item => {
+	item.classList.add('hidden');
+	if (item.dataset.tabTitle === title) { item.classList.remove('hidden'); }
+});
+
+
+Array.from(tabsNav.children).forEach(item => item.addEventListener('click', selectTab));
+function selectTab(event) {
+	Array.from(tabsNav.children).forEach(item => item.classList.remove('ui-tabs-active'));
+	event.currentTarget.classList.add('ui-tabs-active');
+	let title = event.currentTarget.firstElementChild.textContent;
+	Array.from(articleList).forEach(item => {
+		item.classList.add('hidden');
+		if (item.dataset.tabTitle === title) { item.classList.remove('hidden'); }
+	});
+}
