f2151e765d01ddd26083c630d285d738ba62a8b2
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-14 23:04:59 +0300

    created table task

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  const target = event.target;
+  if (!target.classList.contains('prop__name')) { return; }
+  const field = target.getAttribute('data-prop-name');
+  if (!target.dataset.dir) {
+  	target.dataset.dir = 1;
+  } else {
+  	target.dataset.dir *= -1;
+  }
+  document.getElementsByTagName('table')[0].dataset.sortBy = field;
+  sortTable(field, target.dataset.dir);
 }
