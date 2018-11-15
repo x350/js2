656d7670e251a08fb54e9cd644cd7099f71dea32
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-15 18:16:19 +0300

    created cart task

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const shop = document.querySelector('.items-list');
+shop.addEventListener('click', shopping);
+function shopping(event) {
+	if (!event.target.classList.contains('add-to-cart')) { return; }
+	addToCart(event.target.dataset);
+}
