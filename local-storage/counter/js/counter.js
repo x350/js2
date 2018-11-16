5f5dae38189df6789e23d3f82fb31469a507175b
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-16 22:03:22 +0300

    created counter task

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const counter = document.getElementById('counter');
+let count = localStorage.count ? localStorage.count : 0;
+counter.textContent = count;
+const buttonsBoard = document.querySelector('.wrap-btns');
+buttonsBoard.addEventListener('click', event => {
+	
+	if (event.target.id === 'increment') {
+		count++;
+	} else if ( event.target.id === 'decrement') {
+		count--;
+		if (count < 0 ) { count = 0; }
+	} else if (event.target.id === 'reset') {
+		count = 0;
+	} else { return; }
+	localStorage.count = count;
+	counter.textContent = count;
+})
