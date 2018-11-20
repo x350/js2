28db35e8869407ee19f4ce4ab676771b746ebba1
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-20 14:46:50 +0300

    create counter task

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const counter = document.querySelector('.counter');
+const  countErrors = document.querySelector('.errors');
+const url = 'wss://neto-api.herokuapp.com/counter';
+const connection = new WebSocket(url);
+
+window.addEventListener('beforeunload', () => {
+	connection.close(1000);
+});
+
+connection.addEventListener('message', obtainMessage);
+function obtainMessage(event) {
+    const data = JSON.parse(event.data);
+    console.log(data);
+    counter.textContent = data.connections;
+    countErrors.textContent = data.errors;
+}
