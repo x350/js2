29e52c24f2a888e9fff5dc14d4ac7f0d34e5008f
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-20 11:27:20 +0300

    made click task

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const url = 'wss://neto-api.herokuapp.com/mouse';
+const connection = new WebSocket(url);
+showBubbles(connection);
+document.addEventListener('click', sendMessage);
+function sendMessage(event) {
+	const clickObject = {};
+	clickObject['x'] = event.pageX;
+	clickObject['y'] = event.pageY;
+	connection.send(JSON.stringify(clickObject));
+}
+window.addEventListener('beforeunload', () => {
+	connection.close();
+})
