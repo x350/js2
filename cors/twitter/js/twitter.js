30e6d0bc07185947c15443159edec8087fcc5175
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-23 18:42:34 +0300

    made twitter task

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const url = 'https://neto-api.herokuapp.com/twitter/jsonp' + '?' + 'callback=receiveData';
+function receiveData(data) {
+	document.querySelector('[data-wallpaper]').src = data.wallpaper;
+	document.querySelector('[data-username]').textContent = data.username;
+	document.querySelector('[data-description]').textContent = data.description;
+	document.querySelector('[data-pic]').src = data.pic;
+	document.querySelector('[data-tweets]').textContent = data.tweets;
+	document.querySelector('[data-followers]').textContent = data.followers;
+	document.querySelector('[data-following]').textContent = data.following;
+}
+
+function addScript(url) {
+	const script = document.createElement("script");
+	script.src = url;
+	document.body.appendChild(script);
+}
+addScript(url);
+
+// function loadData(url) {
+// 	return new Promise((done, fail) => {
+// 		window.receiveData = done;
+// 	    const script = document.createElement("script");
+// 	    script.type = "text/javascript";
+// 	    script.src = url;
+// 	    document.body.appendChild(script);
+// 	    console.log(window.receiveData);
+// 	    	});
+// }  
+// loadData(url).then(receiveData);
