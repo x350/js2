cf511951b94edc14797fedb8ade4b5c0f1a6f5a3
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-23 19:40:13 +0300

    made profile task

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const url = {};
+url.profile = 'https://neto-api.herokuapp.com/profile/me' + '?callback=getProfile',
+url.techno = 'https://neto-api.herokuapp.com/profile/:id/technologies' + '?callback=getTechno'
+const insertTag = '<span class="devicons "></span>';
+const boxForTech = document.querySelector('[data-technologies]');
+boxForTech.insertAdjacentHTML('beforeEnd', insertTag);
+const insertNode = boxForTech.removeChild(boxForTech.lastElementChild);
+
+function getProfile(data) {
+	document.querySelector('[data-name]').textContent = data.name;
+	document.querySelector('[data-description]').textContent = data.description;
+	document.querySelector('[data-pic]').src = data.pic;
+	document.querySelector('[data-position]').textContent = data.position;
+	const urlRequestTechno = url.techno.replace(':id', data.id);
+	addScript(urlRequestTechno);
+}
+
+function getTechno(data) {	
+	data.forEach( item => {
+		const currentNode = insertNode.cloneNode(true);
+		currentNode.classList.add('devicons-' + item);
+		boxForTech.appendChild(currentNode);
+	})
+	document.querySelector('.content').style.display = 'initial';
+}
+
+function addScript(url) {
+	const script = document.createElement("script");
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+addScript(url.profile);
