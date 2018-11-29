b58447d881b40c383697bf71baa23efeda01e949
Dmitrii Shulyak <x350sh@gmail.com>
2018-11-29 22:29:45 +0300

    made dom task

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function createElement(block) {
+	if (block === undefined || block === null || block === false) {
+		return document.createTextNode('');
+	}
+	if (typeof block === 'string' || typeof block === 'number' || block === true) {
+		return document.createTextNode(block);
+	}
+	if (Array.isArray(block)) {
+		return block.reduce((f, elem) => {
+			f.appendChild(createElement(elem));
+			return f;
+		}, document.createDocumentFragment());
+	}
+
+	const element = document.createElement(block.name || 'div');
+	
+	if (block.props) {
+		element.className = block.props.class;
+	}
+
+	if (block.childs) {
+		element.appendChild(createElement(block.childs));
+	}
+	return element;
+}
