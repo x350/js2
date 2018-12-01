51fd5a4f519e9228c99b68d346e6499482d748df
Dmitrii Shulyak <x350sh@gmail.com>
2018-12-01 16:05:14 +0300

    update slider

--- html-document-structure/slider/js/slider.js
+++ html-document-structure/slider/js/slider.js
@@ -2,71 +2,56 @@
 
 const navigationButtons =  document.querySelector('.slider-nav').children;
 const slides = document.querySelector('.slides');
+const prev = document.querySelector('[data-action="prev"]');
+const next = document.querySelector('[data-action="next"]');
+const first = document.querySelector('[data-action="first"]');
+const last = document.querySelector('[data-action="last"]');
+
 slides.firstElementChild.classList.add('slide-current');
-document.querySelector('a[data-action=prev]').classList.add('disabled');
-document.querySelector('a[data-action=first]').classList.add('disabled');
+prev.classList.add('disabled');
+first.classList.add('disabled');
 
-Array.from(navigationButtons).forEach(item => { 
-	switch (item.dataset.action) {
-		case 'prev': 
-			item.addEventListener('click', prevSlide);
-			break;
-		case 'next':
-			item.addEventListener('click', nextSlide);
-			break;		
-		case 'first':
-			item.addEventListener('click', firstSlide);
-			break;	
-		case 'last':
-			item.addEventListener('click', lastSlide);
-			break;					
-	}
-});
-
-function prevSlide(event) {
+Array.from(navigationButtons).forEach(item => 
+			item.addEventListener('click', getSlide));
+				
+function getSlide(event) {
 	if (event.currentTarget.classList.contains('disabled')) { return; }
-	let currentSlide = document.querySelector('.slide-current');
-	let prevSlide = currentSlide.previousElementSibling;
-	if (prevSlide) {
-		currentSlide.classList.remove('slide-current');
-		prevSlide.classList.add('slide-current');
-		Array.from(navigationButtons).forEach(item => item.classList.remove('disabled'));
-		if (!prevSlide.previousElementSibling) {
-			event.currentTarget.classList.add('disabled');
-			document.querySelector('a[data-action=first]').classList.add('disabled');			
-		}
-	}
-}
-
-function nextSlide() {
-	if (event.currentTarget.classList.contains('disabled')) { return; }
-	let currentSlide = document.querySelector('.slide-current');
-	let nextSlide = currentSlide.nextElementSibling;
-	if (nextSlide) {
-		currentSlide.classList.remove('slide-current');
-		nextSlide.classList.add('slide-current');
-		Array.from(navigationButtons).forEach(item => item.classList.remove('disabled'));
-		if (!nextSlide.nextElementSibling) {
-			event.currentTarget.classList.add('disabled');
-			document.querySelector('a[data-action=last]').classList.add('disabled');			
-		}
-	}
-}
-
-function firstSlide(event) {
-	if (event.currentTarget.classList.contains('disabled')) { return; }
-	Array.from(slides.children).forEach(item => item.classList.remove('slide-current'));
-	slides.firstElementChild.classList.add('slide-current');
+	let currentSlide = document.querySelector('.slide-current');	
 	Array.from(navigationButtons).forEach(item => item.classList.remove('disabled'));
-	event.currentTarget.classList.add('disabled');
-	document.querySelector('a[data-action=prev]').classList.add('disabled');
-}
-
-function lastSlide(event) {
-	if (event.currentTarget.classList.contains('disabled')) { return; }
 	Array.from(slides.children).forEach(item => item.classList.remove('slide-current'));
-	slides.lastElementChild.classList.add('slide-current');
-	Array.from(navigationButtons).forEach(item => item.classList.remove('disabled'));
-	event.currentTarget.classList.add('disabled');
-	document.querySelector('a[data-action=next]').classList.add('disabled');
+	switch (event.currentTarget.dataset.action) {
+			case 'prev': 
+				let prevSlide = currentSlide.previousElementSibling;
+				if (prevSlide) {
+					prevSlide.classList.add('slide-current');					
+					if (!prevSlide.previousElementSibling) {
+						prev.classList.add('disabled');
+						first.classList.add('disabled');			
+					}
+				}
+				break;
+
+			case 'next':
+				let nextSlide = currentSlide.nextElementSibling;
+				if (nextSlide) {
+					nextSlide.classList.add('slide-current');
+					if (!nextSlide.nextElementSibling) {
+						next.classList.add('disabled');
+						last.classList.add('disabled');			
+					}
+				}
+				break;		
+
+			case 'first':
+				slides.firstElementChild.classList.add('slide-current');
+				first.classList.add('disabled');
+				prev.classList.add('disabled');
+				break;
+
+			case 'last':
+				slides.lastElementChild.classList.add('slide-current');
+				last.classList.add('disabled');
+				next.classList.add('disabled');
+				break;					
+			}	
 }
