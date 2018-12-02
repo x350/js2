0408da9725cb0c9e65a2eca88061d5aad44aae7f
Dmitrii Shulyak <x350sh@gmail.com>
2018-12-02 20:25:51 +0300

    create socket task

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,16 @@
 'use strict';
+const url = 'wss://neto-api.herokuapp.com/draw';
+var connection = new WebSocket(url);
+connection.addEventListener('open', () => console.log('Connection opened.'));
+connection.addEventListener('message', event => console.log(event.data));
+
+window.editor.addEventListener('update', event => {
+	event.canvas.toBlob(blob => connection.send(blob));
+	// var bb = new Blob([event.canvas], { type: 'image/jpeg' });
+	// connection.send(bb);
+});
+
+
+connection.addEventListener('error', error => console.log(error.data));
+connection.addEventListener('close', event => console.log(event.code));
+window.addEventListener('beforeunload', connection.close(1000));
